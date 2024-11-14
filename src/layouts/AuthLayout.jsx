0db73d00c820e5c3483b194fff3e0e62f65c5743
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-11-14 21:48:54 +0600

    login & register page design completed

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navber from "../components/Navber";
 
 const AuthLayout = () => {
     return (
-        <div>
+        <div className="font-poppins bg-[#f3f3f3]">
+            <header className="py-3 w-11/12 mx-auto max-w-screen-2xl">
+                <Navber/>
+            </header>
             <Outlet/>
         </div>
     );
